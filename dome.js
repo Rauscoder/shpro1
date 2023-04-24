ed28197a715f38a4c400cd6ff5332a713ced65a8
Rauscoder <raushanrajsharma724@gmail.com>
2023-04-24 15:28:44 +0530

    js code for dom manipulation

---
+++ dome.js
@@ -0,0 +1,26 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title=123;
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10].textcontent);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+//Get element by Id//
+//console.log(document.getElementById('header-title'));
+ var headert=document.getElementById('header-title');
+ var header =document.getElementById('main-header');
+ console.log(headert.innerText);
+  //headert.textContent="hello";
+  //headert.innerText="goodbye";
+  //headert.innerHTML='<h3>hello</h3>';
+  header.style.borderBottom="solid 3px #000 ";
+  var items=document.getElementsByClassName("title");
+  items[0].style.fontWeight="bold";
+  items[0].style.color="green";
+
+
