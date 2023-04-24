e2d21eb0cb63710071c660fdbff4133dfce5ea68
Rauscoder <raushanrajsharma724@gmail.com>
2023-04-25 00:43:02 +0530

    add item and delete button dom code

--- dome.js
+++ dome.js
@@ -69,52 +69,86 @@
 //     oddItems[i].style.backgroundColor='green';
 // }
 //Trasversing the parentnode,childnode and siblingnode//
-var itemList=document.querySelector('#items');
-//parentelement
-console.log(itemList.parentElement);
-itemList.parentElement.style.backgroundColor="yellow";
-//lastChild
-console.log(itemList.lastChild);
-//lastElementChild
-console.log(itemList.lastElementChild);
-//firstChild
-console.log(itemList.firstChild);
-//firstElementChild
-console.log(itemList.firstElementChild);
-itemList.firstElementChild.textContent="Hello 1";
-//nextSibling
-console.log(itemList.nextSibling);
-//nextElementSibling
-console.log(itemList.nextElementSibling);
-//previousSibling
-console.log(itemList.previousSibling);
-//previousElementSibling
-console.log(itemList.previousElementSibling);
-//createElement
-//create a div
-var newDiv=document.createElement('div');
-newDiv.className='hello';
-newDiv.id='hello1';
-newDiv.setAttribute('title','Hello Div');
-//create textnode
-var newDivText=document.createTextNode('Hello World');
-//Add text to div
-newDiv.appendChild(newDivText);
-//add Hello world before itemlister
-var container=document.querySelector('header .container');
-console.log(container);
-var h1=document.querySelector('header h1');
-container.insertBefore(newDiv,h1);
-newDiv.style.fontSize='30px';
-console.log(newDiv);
-//add Hello world before item 1
-var ulList=document.querySelector('ul ');
-console.log(ulList);
-var item1=document.querySelector('ul li:nth-child(1)');
-console.log(item1);
-ulList.insertBefore(newDiv,item1);
+// var itemList=document.querySelector('#items');
+// parentelement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor="yellow";
+// lastChild
+// console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// firstChild
+// console.log(itemList.firstChild);
+// firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent="Hello 1";
+// nextSibling
+// console.log(itemList.nextSibling);
+// nextElementSibling
+// console.log(itemList.nextElementSibling);
+// previousSibling
+// console.log(itemList.previousSibling);
+// previousElementSibling
+// console.log(itemList.previousElementSibling);
+// createElement
+// create a div
+// var newDiv=document.createElement('div');
+// newDiv.className='hello';
+// newDiv.id='hello1';
+// newDiv.setAttribute('title','Hello Div');
+// create textnode
+// var newDivText=document.createTextNode('Hello World');
+// Add text to div
+// newDiv.appendChild(newDivText);
+// add Hello world before itemlister
+// var container=document.querySelector('header .container');
+// console.log(container);
+// var h1=document.querySelector('header h1');
+// container.insertBefore(newDiv,h1);
+// newDiv.style.fontSize='30px';
+// console.log(newDiv);
+// add Hello world before item 1
+// var ulList=document.querySelector('ul ');
+// console.log(ulList);
+// var item1=document.querySelector('ul li:nth-child(1)');
+// console.log(item1);
+// ulList.insertBefore(newDiv,item1);
+//Delete and Edit button functionality
+var forms=document.getElementById('addForm');
 
-
+forms.addEventListener('submit',addItem);
+var itemList=document.getElementById('items');
+console.log(itemList);
+function addItem(e){
+    e.preventDefault();
+    console.log(e.target);
+    var input=document.getElementById('item').value;
+    var li=document.createElement('li');
+    li.className='list-group-item';
+    var text=document.createTextNode(input);
+    li.appendChild(text);
+    var editBtn=document.createElement('button');
+    editBtn.className='btn btn-danger btn-sm float-right delete ';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    li.appendChild(editBtn);
+    console.log(li);
+     var delBtn=document.createElement('button');
+    delBtn.className='btn btn-danger btn-sm float-right delete mr-2';
+    delBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(delBtn);
+    itemList.appendChild(li);
+    }
+    itemList.addEventListener('click',removeItem);
+    
+    function removeItem(e){
+        console.log(e.target);
+        if(e.target.classList.contains('delete')){
+            if(confirm('Are you sure?')){
+              var list=e.target.parentElement;
+              itemList.removeChild(list);  
+            }
+        }
+    }
 
 
 
